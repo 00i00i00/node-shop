324bd0277d58eccebd153fc011d5e4ae066c0a32
00i00i00 <ayaka.hibino@gmail.com>
2022-07-05 23:02:40 -0400

    Adds pages that replaces dummy markup response

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello</h1>'); // utility function to send response, body with any Type. default content=type is text/html
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;

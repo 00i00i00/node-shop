049ee8f552c61d16270ce17e7623954424f6c199
00i00i00 <ayaka.hibino@gmail.com>
2022-07-05 22:32:12 -0400

    updates form path to follow the new filtered path

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   console.log('In middleware');
-  res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 // /admin/add-product => POST

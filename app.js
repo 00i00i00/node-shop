bc59c0cbd0aafeef88d836ecf7b843903d8af6f0
00i00i00 <ayaka.hibino@gmail.com>
2022-08-07 23:09:11 -0400

    Replaces view engine and files with ejs

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views'); // this is set as the default so we don't need to explicitly state this
 
 const adminData = require('./routes/admin');

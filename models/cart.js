a05c4b7c1072f64d037944bf98b5f026f50d3cd9
00i00i00 <ayaka.hibino@gmail.com>
2022-08-15 23:37:01 -0400

    Delete product functionality

--- models/cart.js
+++ models/cart.js
@@ -8,9 +8,9 @@ const p = path.join(
 );
 
 module.exports = class Cart {
-  static addProduct(id, productPrice) {
+  static addProduct (id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
-      let cart = {products: [], totalPrice: 0};
+      let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
@@ -23,13 +23,30 @@ module.exports = class Cart {
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
       } else {
-        updatedProduct = { id: id, qty: 1};
+        updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
       fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
-      })
+      });
+    });
+  }
+
+  static deleteProduct (id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
     });
   }
 };
